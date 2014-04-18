1da8bb9dad80045ebde62d57f0a72d72b6e5bd60
marcinbaranski2223@gmail.com <marcinbaranski2223@gmail.com@4d2ae7d2-d95d-b64e-a80a-59bd06e893e5>
2014-04-18 10:13:02 +0000

    Dzialajace przesuwanie na aktualna lokazlizacje przy starcie mapy

--- trunk/ArivedSMS/src/com/wat/arrivedsms/MapShow.java
+++ trunk/ArivedSMS/src/com/wat/arrivedsms/MapShow.java
@@ -1,36 +1,77 @@
 package com.wat.arrivedsms;
 
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.widget.Toast;
 
-import com.wat.arrivedsms.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.maps.MapController;
 
-	
 
+
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class MapShow extends FragmentActivity{
-	
-	private GoogleMap mMap;
+
+	MapView mapView;
+	MapController mController;
+	LatLng currentLocation;
+
+	//	private static  LatLng currentLocation = new LatLng(52.187 , 21.557) ;
+	private GoogleMap googleMap;
 	//MapActivity,
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapshow);
-		
-		
-//		SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//		mMap = supportMapFragment.getMap();
-	        if (mMap== null)
-	        {
-	            Toast.makeText(this,"Google Maps not Available",
-	                    Toast.LENGTH_LONG).show();
-	        }
-		
+
+		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		LocationListener llistener = new mylocationlistener();
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, llistener);
+
+		googleMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+		//Pobranie aktualnej lokalizacji
+		Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());      
+		//Przesuniecie na aktualna lokalizacje
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		// Zoom mapy
+		googleMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+
+
 	}
+	private class mylocationlistener implements LocationListener {
+		@Override
+		public void onLocationChanged(Location location) {
 
+		}
 
-	
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+
+		}
+	}
 }
