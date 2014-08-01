1e803450ad636c70b0cb6cdd25c4126763f4c22a
marcinbaranski2223 <marcinbaranski2223@gmail.com>
2014-08-01 13:10:31 +0000

    Sygnalizacja gps, brak wyswietlania sie nazw kontaktow.Dzialajaca mapa i jej funkcjonalnosci

--- ArivedSMS/src/com/wat/arrivedsms/LocationMessage.java
+++ ArivedSMS/src/com/wat/arrivedsms/LocationMessage.java
@@ -2,128 +2,293 @@ package com.wat.arrivedsms;
 
 
 
-import com.wat.arrivedsms.R;
-
 import android.app.Activity;
-import android.app.PendingIntent;
+import android.app.Dialog;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class LocationMessage extends Activity {
-	
+
 	private EditText input;
 	private Button getInfo;
 
-	String number;
+	public static Location locationMapView;
+	LocationManager lm;
+
 	String numberK="663992176";
 	String numberM="512238500";
 	String text="Dotar≥em!";
-	Double latDefin=52.187;
-	Double longDefin=21.557;
+	Double latDefin = 0.0;
+	Double longDefin = 0.0;
+	double tmp = 0.0;
 	Integer count;
-	PendingIntent pendingIntent;
 
 	Button btnSignIn,btnSignUp;
-    LoginDataBaseAdapter loginDataBaseAdapter;
-	
+	ImageView kropka_zielona,kropka_czerwona;
+	LoginDataBaseAdapter loginDataBaseAdapter;
+	TextView result;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		count=0;
-		
-		LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	    LocationListener ll = new mylocationlistener();
-	    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
-	
-		 input = (EditText)findViewById(R.id.number);
-		 getInfo = (Button)findViewById(R.id.button1);
-		 getInfo.setOnClickListener(new OnClickListener() {
+		search();
 
-	        @Override
-	        public void onClick(View v) {
-	        	 Intent i = new Intent(getApplicationContext(), MapShow.class);
-         		 startActivity(i);
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		LocationListener ll = new mylocationlistener();
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
 
-	        }
-	    });
+		kropka_zielona = (ImageView) findViewById(R.id.kropka_zielona);
+		kropka_czerwona = (ImageView) findViewById(R.id.kropka_czerwona);
+
+		//Status GPS_kropka
+		if(locationMapView != null){
+
+			kropka_czerwona.setVisibility(View.INVISIBLE);
+			kropka_zielona.setVisibility(View.VISIBLE);
+
+		}
+
+		input = (EditText)findViewById(R.id.number);
+		result = (TextView)findViewById(R.id.searchResult);
+		getInfo = (Button)findViewById(R.id.button1);
+		getInfo.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if((checkEnableGPS()==true) && (isConnectingToInternet()==true && locationMapView != null ) ){
+					Intent i = new Intent(getApplicationContext(), MapShow.class);
+					startActivity(i);
+				}
+				else if((checkEnableGPS()==false) || (isConnectingToInternet()==false)){
+					Toast.makeText(LocationMessage.this, "Wlacz internet i lokalizacje!",Toast.LENGTH_LONG ).show();
+				}
+				else if(locationMapView == null){
+					final Dialog dialog = new Dialog(LocationMessage.this);
+					dialog.setContentView(R.layout.dialog_lokalizacja);
+					dialog.setTitle("èrÛdlo poloøenia dla mapy ?");
+
+					// Ustawienie wartosci dla komponentow dialogu (text)
+					TextView text = (TextView) dialog.findViewById(R.id.text);
+					text.setText("Brak aktualnej pozycji GPS. Wczytac ostatnie znane poloøenie?");
+
+					Button buttonYes = (Button) dialog.findViewById(R.id.dialogLokalizButtonYes);
+					Button buttonNo = (Button) dialog.findViewById(R.id.dialogLokalizButtonNo);
+
+					dialog.show();
+					buttonNo.setOnClickListener(new OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+
+							dialog.dismiss();
+							Toast.makeText(LocationMessage.this, "Czekam na sygnal GPS",Toast.LENGTH_LONG ).show();
+
+						}
+					});
+
+					buttonYes.setOnClickListener(new OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+							dialog.dismiss();
+							locationMapView = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+							if(locationMapView == null){
+								Toast.makeText(LocationMessage.this, "Brak zapisanej lokalizacji.Poczekaj na pobranie aktualnego polozenia.", Toast.LENGTH_SHORT).show();
+							}
+							else{
+								Intent i = new Intent(getApplicationContext(), MapShow.class);
+								startActivity(i);
+							}
+						}
+					});
+
+
+					//Toast.makeText(LocationMessage.this, "Poczekaj na pobranie aktualnej lokalizacji",Toast.LENGTH_SHORT).show();
+				}
+			}
+
+
+
+		});
 	}
 	private class mylocationlistener implements LocationListener {
-		   @Override
-		    public void onLocationChanged(Location location) {
-		        
-			   if(count==0){
-				  
-				   double distance=distancee(latDefin,longDefin,location.getLatitude(),location.getLongitude());
-		        	if(distance< 50){
-		        		 count=1;
-		        		
-		        		try {
-		 					SmsManager smsManager = SmsManager.getDefault();
-		 					smsManager.sendTextMessage(numberK, null, text, null, null);
-		 					Toast.makeText(getApplicationContext(), "SMS Sent!",
-		 								Toast.LENGTH_SHORT).show();
-		 					
-		 				  } catch (Exception e) {
-		 					Toast.makeText(getApplicationContext(),
-		 						"SMS faild, please try again later!",
-		 						Toast.LENGTH_LONG).show();
-		 					e.printStackTrace();
-		 				  }
-		        	}
-//		        	else if (distance>50){
-//		        		count=0;
-//		        	}
-		        	
-			   }
-		   }
-		     
-		   @Override
-		    public void onProviderDisabled(String provider) {
-		    }
-		    @Override
-		    public void onProviderEnabled(String provider) {
-		    }
-		    @Override
-		    public void onStatusChanged(String provider, int status, Bundle extras) {
-		    }
+		@Override
+		public void onLocationChanged(Location  location) {
+
+			if(location != null){
+				locationMapView = location;
+				//status GPS_kropka
+
+				if(locationMapView != null){
+
+					kropka_czerwona.setVisibility(View.INVISIBLE);
+					kropka_zielona.setVisibility(View.VISIBLE);
+				}
+				else{
+					
+					kropka_czerwona.setVisibility(View.VISIBLE);
+					kropka_zielona.setVisibility(View.INVISIBLE);
+				}	
+
+			}
+			if(count==0){
+
+				double distance=distancee(latDefin,longDefin,location.getLatitude(),location.getLongitude());
+				if(distance< 50){
+					count=1;
+
+					try {
+						SmsManager smsManager = SmsManager.getDefault();
+						smsManager.sendTextMessage(numberK, null, text, null, null);
+						Toast.makeText(getApplicationContext(), "SMS Sent!",
+								Toast.LENGTH_SHORT).show();
+
+					} catch (Exception e) {
+						Toast.makeText(getApplicationContext(),
+								"SMS faild, please try again later!",
+								Toast.LENGTH_LONG).show();
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}
+
+		@Override
+		public void onProviderDisabled(String provider) {
+		}
+		@Override
+		public void onProviderEnabled(String provider) {
+		}
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
 	}	
-	
+
 	// Zwraca odleg≥oúÊ w metrach
 	private double distancee(double lat1, double lon1, double lat2, double lon2) {
-			  double theta = lon1 - lon2;
-			  double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
-			  dist = Math.acos(dist);
-			  dist = rad2deg(dist);
-			  dist = dist * 60 * 1.1515;
-			  dist = dist * 1609.344;
-			  return dist;
-			  
+		double theta = lon1 - lon2;
+		double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+		dist = Math.acos(dist);
+		dist = rad2deg(dist);
+		dist = dist * 60 * 1.1515;
+		dist = dist * 1609.344;
+		return dist;
+
 	}
-			
+
 	private double deg2rad(double deg) {
 		return (deg * Math.PI / 180.0);
-		}
-	
+	}
+
 	private double rad2deg(double rad) {
-			  return (rad * 180 / Math.PI);
-			}
+		return (rad * 180 / Math.PI);
+	}
 
 	@Override
 	public void onDestroy(){
 		super.onDestroy();
 	}
+
+
+	@Override
+	protected void onRestart() {
+		if(MapShow.mapShowed=="true"){
+			LatLng positionLatLng = MapShow.marker.getPosition();
+			latDefin = positionLatLng.latitude;
+			longDefin = positionLatLng.longitude;
+		}
+		super.onRestart();
+	}
+
+	public boolean isConnectingToInternet(){
+		ConnectivityManager connectivity = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (connectivity != null) 
+		{
+			NetworkInfo[] info = connectivity.getAllNetworkInfo();
+			if (info != null) 
+				for (int i = 0; i < info.length; i++) 
+					if (info[i].getState() == NetworkInfo.State.CONNECTED)
+					{
+						return true;
+					}
+
+		}
+		return false;
+	}
+
+	private boolean checkEnableGPS(){
+		String provider = Settings.Secure.getString(getContentResolver(),
+				Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+		if(provider.contains("gps")){
+
+			if(!provider.equals("")){
+				//GPS Enabled
+				return true;
+			}
+
+
+		}
+		return false;
+	}
+
+	
+	private void search(){
+		SearchManager searchMan = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+		final SearchView searchView = (SearchView) findViewById(R.id.search);
+		SearchableInfo searchableInfo = (SearchableInfo) searchMan.getSearchableInfo(getComponentName());
+		searchView.setSearchableInfo(searchableInfo);
+	}
+	
+	private String getDisplayNameForContact(Intent intent) {
+		Cursor phoneCursor = getContentResolver().query(intent.getData(), null, null, null, null);
+		phoneCursor.moveToFirst();
+		int idDisplayName = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+		String name = phoneCursor.getString(idDisplayName);
+		phoneCursor.close();
+		return name;
+	}
 	
 	
+	@Override
+	protected void onNewIntent(Intent intent) {
+		if (ContactsContract.Intents.SEARCH_SUGGESTION_CLICKED.equals(intent.getAction())) {
+			//handles suggestion clicked query
+			String displayName = getDisplayNameForContact(intent);
+			result.setText(displayName);
+		} else if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+			String query = intent.getStringExtra(SearchManager.QUERY);
+			result.setText("should search for query: '" + query + "'...");
+		}
+	}
+	
+	
+
+
+
 }
 
--- ArivedSMS/src/com/wat/arrivedsms/MapShow.java
+++ ArivedSMS/src/com/wat/arrivedsms/MapShow.java
@@ -1,20 +1,39 @@
 package com.wat.arrivedsms;
 
 
+import java.util.List;
+import java.util.Locale;
+
 import android.annotation.TargetApi;
+import android.app.Dialog;
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapController;
 
 
@@ -25,8 +44,14 @@ public class MapShow extends FragmentActivity{
 
 	MapView mapView;
 	MapController mController;
-	LatLng currentLocation;
+	LatLng currentLocationMapShow;
+	EditText mapSearchBox;
 
+	InputMethodManager inputManager;
+	public static MarkerOptions marker;
+	public static String mapShowed;
+	public LatLng markerLatLngPosition;
+	public String search;
 	//	private static  LatLng currentLocation = new LatLng(52.187 , 21.557) ;
 	private GoogleMap googleMap;
 	//MapActivity,
@@ -39,15 +64,146 @@ public class MapShow extends FragmentActivity{
 		LocationListener llistener = new mylocationlistener();
 		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, llistener);
 
+		//Wyszukiwarka
+
+		mapSearchBox = (EditText) findViewById(R.id.mapSearchBox);
+		Button mapSearch = (Button) findViewById(R.id.search);
+		Button mapViewType = (Button) findViewById(R.id.mapTypeChange);
+
+		inputManager = (InputMethodManager)
+				getSystemService(Context.INPUT_METHOD_SERVICE); 
+
+
+
+		mapSearch.setOnClickListener(new OnClickListener() {
+
+
+
+			@Override
+			public void onClick(View v) {
+
+				inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),InputMethodManager.HIDE_NOT_ALWAYS);
+
+				if(mapSearchBox.getText().toString().trim().length()<=0){
+					//Nie wpisano lokalizacji do wyszukiwania wiec nie rob nic
+					Toast.makeText(MapShow.this, "Popraw puste pole!", Toast.LENGTH_SHORT).show();
+				}
+				else{
+					search = mapSearchBox.getText().toString();
+					Address address;
+					try {
+						Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+						List<Address> results = geocoder.getFromLocationName(search, 1);
+
+						if (results.size() == 0) {
+							System.out.println("Error");	
+						}
+
+						address = results.get(0);
+						LatLng searchedLocation = new LatLng(address.getLatitude(), address.getLongitude());
+
+						//Przesuniecie na wyszukiwana lokalizacje
+						googleMap.moveCamera(CameraUpdateFactory.newLatLng(searchedLocation));
+						// Zoom mapy
+						googleMap.animateCamera(CameraUpdateFactory.zoomTo(16));
+
+
+					} catch (Exception e) {
+						Log.e("", "Something went wrong: ", e);
+
+					}
+				}
+			}
+		});
+
+		mapViewType.setOnClickListener(new OnClickListener() {
+			int mapFlag=0;
+			@Override
+			public void onClick(View v) {
+				if(mapFlag == 0){
+					googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+					mapFlag = 1;
+				}
+				else if(mapFlag == 1){
+					googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+					mapFlag = 0;
+				}
+			}
+		});
+
+
+
+
 		googleMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
+
 		//Pobranie aktualnej lokalizacji
-		Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());      
+
+		//			Jezeli aktualna pozycja  jest pusta pobierz ostatnia zapamietana lokalizacje
+		if(LocationMessage.locationMapView == null  ){
+
+			Toast.makeText(MapShow.this, "Poczekaj na pobranie aktualnej lokalizacji.",Toast.LENGTH_SHORT).show();
+			//			Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			//			currentLocationMapShow = new LatLng(location.getLatitude(),location.getLongitude());   
+		}
+		else{
+			currentLocationMapShow = new LatLng(LocationMessage.locationMapView.getLatitude(),LocationMessage.locationMapView.getLongitude());
+		}
+
+		//Pobranie ostatniej znanej lokalizacji
+
+
+		//		Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		//		LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());      
+
+
 		//Przesuniecie na aktualna lokalizacje
-		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocationMapShow));
 		// Zoom mapy
 		googleMap.animateCamera(CameraUpdateFactory.zoomTo(18));
 
+		googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+
+			@Override
+			public void onMapLongClick(LatLng point) {
+				marker = new MarkerOptions().position(new LatLng(point.latitude, point.longitude)).title("New Marker");
+				googleMap.addMarker(marker);
+				mapShowed = "true";
+				Toast.makeText(MapShow.this, "Wskazano lokalizacje!",Toast.LENGTH_LONG ).show();
+
+				final Dialog dialog = new Dialog(MapShow.this);
+				dialog.setContentView(R.layout.dialog);
+				dialog.setTitle("Wybrano lokalizacje");
+
+				// Ustawienie wartosci dla komponentow dialogu (text)
+				TextView text = (TextView) dialog.findViewById(R.id.text);
+				text.setText("Lokalizacja docelowa jest poprawna?");
+
+				Button buttonYes = (Button) dialog.findViewById(R.id.dialogButtonYes);
+				Button buttonNo = (Button) dialog.findViewById(R.id.dialogButtonNo);
+
+				dialog.show();
+				buttonNo.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						googleMap.clear();
+						dialog.dismiss();
+						Toast.makeText(MapShow.this, "Zmien lokalizacje",Toast.LENGTH_LONG ).show();
+
+					}
+				});
+
+				buttonYes.setOnClickListener(new OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
+						dialog.dismiss();
+						finish();
+					}
+				});
+
+			}
+		}); 
 
 	}
 	private class mylocationlistener implements LocationListener {
@@ -71,6 +227,8 @@ public class MapShow extends FragmentActivity{
 		@Override
 		public void onProviderDisabled(String provider) {
 			// TODO Auto-generated method stub
+
+
 
 		}
 	}
