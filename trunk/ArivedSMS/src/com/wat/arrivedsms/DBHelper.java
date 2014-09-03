e1a8d99828c4b7aaa7df971a953f4599c7b21766
marcinbaranski2223@gmail.com <marcinbaranski2223@gmail.com@4d2ae7d2-d95d-b64e-a80a-59bd06e893e5>
2014-09-03 22:21:33 +0000

    Dodany gest do szybkiego logowania + gdy brak nazwy miasta (punkt docelowy z mapy) to wyświetla samą ulice

--- trunk/ArivedSMS/src/com/wat/arrivedsms/DBHelper.java
+++ trunk/ArivedSMS/src/com/wat/arrivedsms/DBHelper.java
@@ -7,31 +7,31 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
-	
+
 	public DBHelper(Context context, String name, CursorFactory factory , int version)
 	{
-		 super(context, name, factory, version);
+		super(context, name, factory, version);
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase _db) {
 		// TODO Auto-generated method stub
-		 _db.execSQL(LoginDataBaseAdapter.DATABASE_CREATE);
+		_db.execSQL(LoginDataBaseAdapter.DATABASE_CREATE);
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase _db, int _oldVersion, int _newVersion) {
 		// TODO Auto-generated method stub
 		// Log the version upgrade.
-        Log.w("TaskDBAdapter", "Upgrading from version " +_oldVersion + " to " +_newVersion + ", which will destroy all old data");
+		Log.w("TaskDBAdapter", "Upgrading from version " +_oldVersion + " to " +_newVersion + ", which will destroy all old data");
 
-        // Upgrade the existing database to conform to the new version. Multiple
-        // previous versions can be handled by comparing _oldVersion and _newVersion
-        // values.
-        // The simplest case is to drop the old table and create a new one.
-        _db.execSQL("DROP TABLE IF EXISTS " + "TEMPLATE");
-        // Create a new one.
-        onCreate(_db);
-		
+		// Upgrade the existing database to conform to the new version. Multiple
+		// previous versions can be handled by comparing _oldVersion and _newVersion
+		// values.
+		// The simplest case is to drop the old table and create a new one.
+		_db.execSQL("DROP TABLE IF EXISTS " + "TEMPLATE");
+		// Create a new one.
+		onCreate(_db);
+
 	}
 }
