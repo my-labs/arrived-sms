d198a4e08fb7cb8d25bcbbc4ef56af192c9cd83b
marcinbaranski2223@gmail.com <marcinbaranski2223@gmail.com@4d2ae7d2-d95d-b64e-a80a-59bd06e893e5>
2014-11-11 20:02:10 +0000

    Wykomentowane dzialanie do inzyniera

--- trunk/ArivedSMS/src/com/wat/arrivedsms/MainActivity.java
+++ trunk/ArivedSMS/src/com/wat/arrivedsms/MainActivity.java
@@ -24,6 +24,8 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnGesturePerformedListener {
 
+	
+	//do wykasowania- nie uzywane
 	private EditText input;
 	private Button getInfo;
 
@@ -31,14 +33,12 @@ public class MainActivity extends Activity implements OnGesturePerformedListener
 	GestureOverlayView gestView;
 
 	String number;
-	String numberTmp="512238500";
 	String text="Dotar³em!";
 	String storedPassword;
 	Double latDefin=52.187;
 	Double longDefin= 21.557;
 	Integer count=0;
 	PendingIntent pendingIntent;
-
 	Button btnSignIn,btnSignUp;
 	LoginDataBaseAdapter loginDataBaseAdapter;
 
@@ -58,34 +58,28 @@ public class MainActivity extends Activity implements OnGesturePerformedListener
 				Log.e("GestureSignUpActivity", "Nie wczytano biblioteki gestów");
 
 			}
-
 			else{
 				gestView = (GestureOverlayView)findViewById(R.id.gestures);
 				gestView.addOnGesturePerformedListener(this);
 				gestView.setGestureVisible(false);
-
-
 			}
 		}
-
-
-
 
 		final EditText userName=(EditText)findViewById(R.id.editUserNameLogin);
 		userName.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
 		final EditText userPass=(EditText)findViewById(R.id.editPasswordLogin);
 		userPass.setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-		Button loginButton=(Button)findViewById(R.id.buttonLogin);
 
-		loginButton.setOnClickListener(new OnClickListener() {
+		Button loginButton=(Button)findViewById(R.id.buttonLogin);				// Stworzenie przycisku logowania
+		loginButton.setOnClickListener(new OnClickListener() {					//Listener event'ów przycisku logowania
 
 			@Override
 			public void onClick(View v) {
 
-				String userLogin=userName.getText().toString();
-				String password=userPass.getText().toString();
-				if(loginDataBaseAdapter.getSingleEntry(userLogin)==null){
+				String userLogin=userName.getText().toString();					//Pobranie wproawdzonego loginu
+				String password=userPass.getText().toString();					//Pobranie wprowadzonego hasla uzytkownika
 
+				if(loginDataBaseAdapter.getSingleEntry(userLogin)==null){		//
 					storedPassword="admin";
 				}
 				else{
@@ -98,42 +92,34 @@ public class MainActivity extends Activity implements OnGesturePerformedListener
 					startActivity(i);
 				}
 				else
-
 					Toast.makeText(MainActivity.this, "Login lub has³o jest z³e!",Toast.LENGTH_LONG ).show();
 
 			}
 		});
 
 
-		Button buttonRegg=(Button)findViewById(R.id.buttonReg);
-		buttonRegg.setOnClickListener(new OnClickListener() {
-
+		Button buttonRegg=(Button)findViewById(R.id.buttonReg);								//Stworzenie przycisku rejestracji konta	
+		buttonRegg.setOnClickListener(new OnClickListener() {								//Przygotowanie listener'a klikniecia
 			@Override
 			public void onClick(View v) {
-
-				String userNameReg=userName.getText().toString();
-				String passwordReg=userPass.getText().toString();
-
-				boolean checkAvalible=loginDataBaseAdapter.verification(userNameReg);
-
-				if(userNameReg.equals("")||passwordReg.equals(""))
+				String userNameReg=userName.getText().toString();							//Pobranie loginu wprowadzonego do rejestracji
+				String passwordReg=userPass.getText().toString();							//Pobranie hasla wprowadzonego do rejestracji
+				boolean checkAvalible=loginDataBaseAdapter.verification(userNameReg);		//Operacja sprawdzenia dostepnosci
+																							//wprowadzonego loginu
+				if(userNameReg.equals("")||passwordReg.equals(""))							//Zabezpieczenie przed pustymi polami:login i haslo
 				{
-					Toast.makeText(getApplicationContext(), "Zosta³y puste pola!", Toast.LENGTH_LONG).show();
-
-
-					return;
+					Toast.makeText(getApplicationContext(), "Zosta³y puste pola!", Toast.LENGTH_LONG).show(); //Komunikat o pustych polach
+					return;																	//Opuszczenie metody
 				}
-				else if(checkAvalible==true){
-					Toast.makeText(getApplicationContext(), "Login zajêty!", Toast.LENGTH_LONG).show();
-
-
-					return;
+				else if(checkAvalible==true){												//Jezeli login niedostepny(wartosci true)
+					Toast.makeText(getApplicationContext(), "Login zajêty!", Toast.LENGTH_LONG).show(); //Komunikat o zajetosci loginu
+					return;																	//Opuszczenie metody
 				}
 				else
 				{
-					loginDataBaseAdapter.insertEntry(userNameReg, passwordReg);
-					Toast.makeText(getApplicationContext(), "Account is created", Toast.LENGTH_LONG).show();
-				}
+					loginDataBaseAdapter.insertEntry(userNameReg, passwordReg);				//Jezeli login dostepny to wpisanie rekordu do
+					Toast.makeText(getApplicationContext(), "Account is created", Toast.LENGTH_LONG).show(); //danych z wprowadzonym loginem 
+				}																			//haslem oraz wyswietlenie komunikatu
 			}
 		});
 	}
@@ -153,12 +139,8 @@ public class MainActivity extends Activity implements OnGesturePerformedListener
 
 	}
 
-
-
 	@Override
 	public void onDestroy(){
 		super.onDestroy();
 	}
-
-
 }
